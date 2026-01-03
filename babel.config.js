026cae80c5803df65536715c490a427a00b755e2
snamdeo17 <shivendranamdev@gmail.com>
2026-01-03 20:09:20 +0530

    Initialize React Native project with package.json, babel config, and metro config

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,6 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      'react-native-paper/babel',
-    ],
-  };
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+  ],
 };
